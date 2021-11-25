427415111720ddd70581195bb49c756a09705c3f
hamdain <dain9812@naver.com>
2021-11-25 15:21:24 +0900

    docs: Add done to show it's done

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,9 @@
 // Do fizzbuzz with for and if
 // While i goes up 1 to 15,
-// print 'fizz' if i is times of 3
-// print 'buzz' if i is times of 5
+// print 'fizz' if i is times of 3(done)
+// print 'buzz' if i is times of 5(done)
 // print fizzbuzz' if i is time of 15
+// else, print i (done, string)
 for(let i = 1; i<16; i++) {
 	if ( i%3 == 0) {
 		console.log('fizz');
